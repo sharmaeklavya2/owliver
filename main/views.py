b57110f1b7ebb6d14f0cab2b6abcfb0d2f00bc48
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-07-15 13:20:22 +0530

    Added templates and updated about page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import get_version
+
+import sys
+
+def get_python_version():
+	return ".".join([str(sys.version_info.major),str(sys.version_info.minor)])
+def get_django_version():
+	return get_version()[:4]
 
 def about(request):
-	return HttpResponse("Owliver is a site to make and take tests.\nCreated by Eklavya Sharma using Python 3.4 and Django 1.8")
+	context_dict = {}
+	context_dict["python_version"] = get_python_version()
+	context_dict["django_version"] = get_django_version()
+	context_dict["contributors"] = ["Eklavya Sharma"]
+	return render(request,"about.html",context_dict)
