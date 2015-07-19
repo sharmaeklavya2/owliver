21a8e32339506d8b3c5fec6cf65c3bc2fd0c52d1
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-07-20 00:20:27 +0530

    Improved accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^$', views.index, name='index'),
 	url(r'^login/$', views.login_view, name='login'),
 	url(r'^logout/$', views.logout_view, name='logout'),
 	url(r'^register/$', views.register, name='register'),
+	url(r'^account_info/$', views.account_info, name='account_info'),
+	url(r'^user/(?P<username>\w+)/$', views.public_profile, name='public_profile'),
+	url(r'^edit_profile/$', views.edit_profile, name='edit_profile'),
+	url(r'^change_password/$', views.change_password, name='change_password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import EditProfileForm
 
 def login_view(request):
 	context_dict = {}
 	if request.method=="POST":
+		next_url=""
+		if "next" in request.POST:
+			next_url = request.POST["next"]
+		if next_url:
+			context_dict["next"] = next_url
 		if "username" in request.POST and "password" in request.POST and request.POST["username"] and request.POST["password"]:
 			username = request.POST["username"]
 			password = request.POST["password"]
@@ -20,9 +27,6 @@ def login_view(request):
 				context_dict["login_error"] = "Your account has been disabled. Contact support."
 			else:
 				login(request,user)
-				next_url = ""
-				if "next" in request.POST:
-					next_url = request.POST["next"]
 				if not next_url:
 					next_url = settings.LOGIN_REDIRECT_URL
 				return HttpResponseRedirect(next_url)
@@ -64,4 +68,49 @@ def register(request):
 		else:
 			context_dict["register_error"] = "You must fill out all fields"
 	return render(request,"accounts/register.html",context_dict)
+
+not_impl_string = "This page is not yet implemented"
+
+def index(request):
+	return HttpResponseRedirect(reverse("accounts:account_info"))
+
+@login_required
+def account_info(request):
+	return render(request,"accounts/account_info.html",{})
+
+def public_profile(request,username):
+	puser = get_object_or_404(User,username=username)
+	context_dict = {"puser":puser}
+	return render(request,"accounts/public_profile.html",context_dict)
+
+@login_required
+def edit_profile(request):
+	if request.method=="POST":
+		form = EditProfileForm(request.POST,instance=request.user)
+		if form.is_valid():
+			form.save()
+	else:
+		form = EditProfileForm(instance=request.user)
+	context_dict = {"form":form}
+	return render(request,"accounts/edit_profile.html",context_dict)
+
+@login_required
+def change_password(request):
+	context_dict = {}
+	if request.method=="POST":
+		keys = ("password1","password2")
+		if all((key in request.POST and request.POST[key]) for key in keys):
+			password1 = request.POST["password1"]
+			password2 = request.POST["password2"]
+			if password1 != password2:
+				context_dict["cp_error"] = "Passwords do not match"
+			else:
+				request.user.set_password(password1)
+				request.user.save()
+				user = authenticate(username=request.user.username,password=password1)
+				login(request,user)
+				return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+		else:
+			context_dict["cp_error"] = "You must fill out both fields"
+	return render(request,"accounts/change_password.html",context_dict)
 
