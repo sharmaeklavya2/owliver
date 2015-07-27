0598f2d3c04fa8d513a87faa3f685204329a9d0f
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-07-27 19:43:36 +0530

    Added 404 and 500 error pages

--- project_conf/urls.py
+++ project_conf/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^accounts/', include('accounts.urls',namespace='accounts')),
 	url(r'^', include('main.urls',namespace='main')),
+	url(r'^404/$', TemplateView.as_view(template_name='404.html')),
+	url(r'^500/$', TemplateView.as_view(template_name='500.html')),
 ]
 
 from . import settings
